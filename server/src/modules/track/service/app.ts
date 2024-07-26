a6b6f2319b3543dd060424af2d29623dfe0a61d0
Dubhe <epoll@foxmail.com>
2024-07-26 14:48:57 +0800

    fix: 修改track user缓存逻辑

--- server/src/modules/track/service/app.ts
+++ server/src/modules/track/service/app.ts
@@ -1,4 +1,4 @@
-import { Provide } from '@midwayjs/decorator';
+import { Inject, Provide } from '@midwayjs/decorator';
 import { BaseService } from '@cool-midway/core';
 import * as _ from 'lodash';
 import { AppInfo } from '../entity/track_app_info';
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { InjectEntityModel } from '@midwayjs/typeorm';
 import { Caching } from '@midwayjs/cache-manager';
 import { TrackUser } from '../entity/track_user';
+import { RedisService } from '@midwayjs/redis';
 
 @Provide()
 export class AppInfoService extends BaseService {
@@ -13,6 +14,8 @@ export class AppInfoService extends BaseService {
   appInfoEntity: Repository<AppInfo>;
   @InjectEntityModel(TrackUser)
   trackUserEntity: Repository<TrackUser>;
+  @Inject()
+  redis: RedisService;
 
   @Caching('default', 'appTrackInfo', 1000 * 60)
   async getInfo(appKey: string) {
@@ -24,13 +27,22 @@ export class AppInfoService extends BaseService {
     return appInfo;
   }
 
-  @Caching('default', 'appTrackUser', 1000 * 60)
   async getUser(appKey: string, userId: string) {
-    const user = await this.trackUserEntity.findOneBy({
-      appKey,
-      userId,
-    });
+    const key = `${appKey}:user:${userId}`;
+    const result = await this.redis.get(key);
+    if (!result) {
+      const user = await this.trackUserEntity.findOneBy({
+        appKey,
+        userId,
+      });
 
-    return user;
+      if (user) {
+        await this.redis.set(key, JSON.stringify(user), 'EX', 60 * 5);
+      }
+      return user;
+    } else {
+      return JSON.parse(result);
+    }
   }
 }
+
